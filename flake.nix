db4e1bd86f875586e65ae384249642eda5bd1e0c
DewaldV <dv@dewaldv.com>
2026-02-16 21:58:15 +0000

    fix: remove unused nixpkgs input and fmt

--- flake.nix
+++ flake.nix
@@ -3,13 +3,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in with pkgs; {
-        devShells.default = mkShell rec { nativeBuildInputs = [ go hugo ]; };
-      });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            go
+            hugo
+          ];
+        };
+      }
+    );
 }
